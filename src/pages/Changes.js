9461f71c1a04cd54d4e6a36392036412d8601fbe
samroodAli <devsamrood@gmail.com>
2021-06-23 11:58:59 +0530

    Mdx:Remove next/mdx and use react markdown for preview and render markdown

---
+++ src/pages/Changes.js
@@ -0,0 +1,29 @@
+const review = `
+Hi {name}, 
+
+Good job so far!
+There are some issues that you still need to work on to go to the next project but you are almost there!
+<FEEL FREE TO CHANGE THIS MESSAGE TO YOUR PERSONAL WORDS>
+
+## Required Changes ♻️
+_Check the comments under the review._
+
+## Optional suggestions
+_Every comment with the [OPTIONAL] prefix is not crucial enough to stop the approval of this PR. However, I strongly recommend you to take them into account as they can make your code better._
+
+You can also consider:
+- x
+- y
+- z
+
+Cheers and Happy coding!👏👏👏
+
+Feel free to leave any questions or comments in the PR thread if something is not 100% clear.
+
+**_Please, do not open a new Pull Request for re-reviews. You should use the same Pull Request submitted for the first review, either valid or invalid unless it is requested otherwise._**
+
+------
+_As described in the [Code reviews limits policy](https://microverse.zendesk.com/hc/en-us/articles/1500004088561) you have only 3 code reviews per project. If you think that the code review was not fair, you can request a second opinion using [this form](https://airtable.com/shrQAqnBwek5a0O0s)._
+`;
+
+export default review;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,6 @@
-import { MDXProvider } from "@mdx-js/react";
 import { useState } from "react";
-
-const Name = ({ name }) => <span>{name}</span>;
+import ReactMarkdown from "react-markdown";
+import data from "./Changes";
 
 const Form = ({ name, setName }) => {
   return (
@@ -11,20 +10,16 @@ const Form = ({ name, setName }) => {
 
 const Index = function Index() {
   const [name, setName] = useState("");
+  const input = () => {
+    return data.replace("{name}", name);
+  };
   return (
     <>
       <Form name={name} setName={setName} />
       <p>
-        Hi <Name name={name} />, Your project is complete! There is nothing else
-        to say other than... it's time to merge it :shipit: Congratulations! 🎉
-        Cheers and Happy coding!👏👏👏 Feel free to leave any questions or
-        comments in the PR thread if something is not 100% clear. ------ _As
-        described in the [Code reviews limits
-        policy](https://microverse.zendesk.com/hc/en-us/articles/1500004088561)
-        you have only 3 code reviews per project. If you think that the code
-        review was not fair, you can request a second opinion using [this
-        form](https://airtable.com/shrQAqnBwek5a0O0s)._
+        <ReactMarkdown>{input()}</ReactMarkdown>
       </p>
+      <textarea style={{ height: "50vh", width: "50%" }} value={input()} />
     </>
   );
 };
